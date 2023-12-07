8d7379f901be204f64008c8ecbe211ec88ac11bd
Arthur Ribeiro Dias Fudali <arthurfudali5@gmail.com>
2023-12-06 22:38:29 -0300

    funcionamento do python

---
+++ models/database.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Classe responsável por criar a entidade "Estudante" com os atributos: id, nome e idade. 
+""" class Estudante(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(150))
+    idade = db.Column(db.Integer)
+    
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade """
+
+class Produto(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    imagem = db.Column(db.String(50))
+    nome = db.Column(db.String(150))
+    descricao = db.Column(db.String[500])
+    valor = db.Column(db.Float)
